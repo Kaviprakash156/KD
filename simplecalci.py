ca587b69726e9465d3b2bee302bbd4238ac747e6
Kaviprakash156 <36467048+Kaviprakash156@users.noreply.github.com>
2018-02-15 02:13:18 -0800

    Update simplecalci.py

--- simplecalci.py
+++ simplecalci.py
@@ -1,26 +1,28 @@
-x=int(input("enter the number1:"))
-y=int(input("enter the number2:"))
-K=int(input("chose the option \n 1]addition\n 2]subtraction\n 3]multiplication\n 4]division\n 5]power\n 6]square of x\n 7]square of y\n 8]cube of x\n 9]cube of y\n 10]modulus\n\nOperation for="))
+x=float(input("enter the number1:"))
+y=float(input("enter the number2:"))
+K=int(input("chose the option \n 1]addition\n 2]subtraction\n 3]multiplication\n 4]division\n 5]power\n 6]square of x\n 7]square of y\n 8]cube of x\n 9]cube of y\n 10]modulus\n\nyour option is:"))
 if (K==1):
-  print("your addition is",x+y)
+  print("\nyour addition is",x+y)
 elif (K==2):
-  print("subtraction",x-y)
+  print("\nsubtraction",x-y)
 elif (K==3):
-  print("multiplication",x*y)
+  print("\nmultiplication",x*y)
 elif(K==4) :
   if (y==0):
-    print("infinite")
+    print("\ninfinite")
   else:
-    print("division",x/y)
+    print("\ndivision",x/y)
 elif(K==5):
-  print("power is",x**y)
+  print("\npower is",x**y)
 elif(K==6):
-  print("square of x is",x**2)
+  print("\nsquare of x is",x**2)
 elif(K==7):
-  print("square of y is",y**2)
+  print("\nsquare of y is",y**2)
 elif(K==8):
-  print("cube of x is",x**3)
+  print("\ncube of x is",x**3)
 elif(K==9):
-  print("cube of y is",y**3)
+  print("\ncube of y is",y**3)
+elif(K==10):
+  print("\nmodulus is",x%y)
 else:
-  print("modulus is",x%y)
+  print("no operation")
